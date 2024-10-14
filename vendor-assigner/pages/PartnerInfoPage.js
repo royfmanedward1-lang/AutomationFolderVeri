2e8529e3b3f0652869a2f567a3b073031d27860b
iventura <iventura@veritext.com>
2024-10-14 12:11:54 -0600

    Implement autocomplete behavior for single partner type selection (#31)
    * Implement autocomplete behavior for single partner type selection
    
    * fixing code for the test case that is failiing
    
    ---------
    
    Co-authored-by: israelgonzalez999 <israel.gonzalez@orthofi.com>

--- vendor-assigner/pages/PartnerInfoPage.js
+++ vendor-assigner/pages/PartnerInfoPage.js
@@ -3,21 +3,71 @@ export class PartnerInfoPage {
         this.page = page
         this.searchText = page.getByPlaceholder('Search Partner Last Name /')
         this.NoFoundOption = page.getByText('No Partner or Agency Found');
-        this.inputFirstName = page.locator(`[name='firstName']`).first()  
+        this.inputFirstName = page.locator(`[name='firstName']`).first()
         this.inputLastName = page.locator(`[name='lastName']`).first()
         this.inputEmail = page.locator(`[name='email']`).first()
         this.inputBillingEmail = page.locator(`[name='billingEmail']`).first()
         this.errorEmail = page.getByText('Enter a valid email address.Example: email@example.com').first()
         this.menu = page.locator('.app > div > div > div').first()
-        this.inputAgencyName = page.locator(`[name='name']`).first() 
+        this.inputAgencyName = page.locator(`[name='name']`).first()
         this.buttonSave = page.getByRole('button', { name: 'Save' })
+        this.inputIndependentContractor = page.getByLabel('Independent Contractor')
+        this.serviceLink = page.getByRole('tab', { name: 'Services' }).nth(1)
+        this.succesNoResults = page.locator('div').filter({ hasText: 'No Results FoundTry a new' }).nth(2)
+    }
 
-    }   
-
+    getServiceLocator = async () => {
+        const serviceLabel = await this.page.locator('label').filter({
+            hasText: 'Service'
+        })
+        const attributeFor = await serviceLabel.getAttribute('for')
+        const serviceSelector = `[id='${attributeFor}']`;
+        return this.page.locator(serviceSelector)
+    }
     selectFirstOption = async () => {
         const searchBar = await this.page.getByRole('option').nth(0)
         await searchBar.click()
-    }   
+    }
+
+    verifyServiceAlphabeticalOrder = async () =>{
+        const serviceArray = await this.getServiceList()
+        return this.verifyAlphabeticalOrder(serviceArray)
+    }
+
+    verifyServiceCharacterContain = async (str) =>{
+        const serviceArray = await this.getServiceList()
+        return this.itemsContainStringIgnoreCase(serviceArray, str)
+    }
+
+    getServiceList = async () => {
+        const serviceLabel = await this.page.locator('label').filter({
+            hasText: 'Service'
+        })
+        const attributeFor = await serviceLabel.getAttribute('for')
+        const serviceArray = [];
+        let index = 0;
+        while (true) {
+            const serviceSelector = `[id='${attributeFor}-option-${index}']`;
+            const serviceItem = await this.page.locator(serviceSelector);
+            
+            if (await serviceItem.count() === 0) {
+                break;
+            }
+            
+            const span = await serviceItem.textContent();
+            serviceArray.push(span.trim());
+            index++;
+        }
+        return serviceArray;
+    }
+
+    verifyAlphabeticalOrder(servicesArray) {
+        const sortedArray = [...servicesArray].sort((a, b) => a.localeCompare(b));
+        return JSON.stringify(servicesArray) === JSON.stringify(sortedArray);
+    }
+
+    itemsContainStringIgnoreCase(array, str) {
+        const lowerCaseStr = str.toLowerCase();
+        return array.every(item => item.toLowerCase().includes(lowerCaseStr));
+    }
 }
-
-
--- vendor-assigner/tests/partnerInformation.spec.js
+++ vendor-assigner/tests/partnerInformation.spec.js
@@ -53,3 +53,76 @@ test('search agency information', async ({page}) => {
     await partnerInfoPage.page.keyboard.press('Backspace')
     await partnerInfoPage.menu.click()
 })
+test('Partner has a type and an email filled ',async ({page})=>{
+    const headerPage = new HeaderPage(page)
+    const partnerInfoPage = new PartnerInfoPage(page)
+    await headerPage.partnerInformationTab.click()
+    await partnerInfoPage.searchText.fill('Alfara')
+    await partnerInfoPage.selectFirstOption()
+    await expect(await partnerInfoPage.inputIndependentContractor).toHaveText('Independent Contractor')
+    await expect(await partnerInfoPage.inputEmail).toHaveValue(/.+/);
+})
+
+test('Service type is selectable ',async ({page})=>{
+    const headerPage = new HeaderPage(page)
+    const partnerInfoPage = new PartnerInfoPage(page)
+    await headerPage.partnerInformationTab.click()
+    await partnerInfoPage.searchText.fill('Alfara')
+    await partnerInfoPage.selectFirstOption()
+    await expect(await partnerInfoPage.inputIndependentContractor).toHaveText('Independent Contractor')
+    await expect(await partnerInfoPage.inputEmail).toHaveValue(/.+/);
+    const partnerTypeCount = page.locator('.MuiBox-root.mui-style-1wmkppu');
+    const lengthValue = await partnerTypeCount.getAttribute('length');
+    expect(lengthValue).toBe('1');
+    await partnerInfoPage.serviceLink.click()
+    const dropDownService = await partnerInfoPage.getServiceLocator()
+    await dropDownService.waitFor({ state: 'visible' });
+    let servicePlaceholder = await dropDownService.getAttribute('placeholder')
+    await expect(servicePlaceholder).toEqual('Select Service')
+    await dropDownService.click()
+    servicePlaceholder = await dropDownService.getAttribute('placeholder')
+    await expect(servicePlaceholder).toEqual('')
+    const areServicesAphabeticalOrder = await partnerInfoPage.verifyServiceAlphabeticalOrder();
+    expect(areServicesAphabeticalOrder).toBeTruthy()
+})
+
+test('Filtering services with a single character search',async ({page})=>{
+    const headerPage = new HeaderPage(page)
+    const partnerInfoPage = new PartnerInfoPage(page)
+    await headerPage.partnerInformationTab.click()
+    await partnerInfoPage.searchText.fill('Alfara')
+    await partnerInfoPage.selectFirstOption()
+    const dropDownService = await partnerInfoPage.getServiceLocator()
+    await dropDownService.fill('R');
+    const areServicesAphabeticalOrder = await partnerInfoPage.verifyServiceAlphabeticalOrder();
+    expect(areServicesAphabeticalOrder).toBeTruthy()
+    const areServicesItemsWithCharacter = await partnerInfoPage.verifyServiceCharacterContain("R");
+    expect(areServicesItemsWithCharacter).toBeTruthy()
+})
+
+
+test('Filtering services with multiple characters search',async ({page})=>{
+    const headerPage = new HeaderPage(page)
+    const partnerInfoPage = new PartnerInfoPage(page)
+    await headerPage.partnerInformationTab.click()
+    await partnerInfoPage.searchText.fill('Alfara')
+    await partnerInfoPage.selectFirstOption()
+    const dropDownService = await partnerInfoPage.getServiceLocator()
+    await dropDownService.fill('CA');
+    const areServicesAphabeticalOrder = await partnerInfoPage.verifyServiceAlphabeticalOrder();
+    expect(areServicesAphabeticalOrder).toBeTruthy()
+    const areServicesItemsWithCharacter = await partnerInfoPage.verifyServiceCharacterContain("CA");
+    expect(areServicesItemsWithCharacter).toBeTruthy()
+})
+
+test('filtering services with empty result',async ({page})=>{
+    const headerPage = new HeaderPage(page)
+    const partnerInfoPage = new PartnerInfoPage(page)
+    await headerPage.partnerInformationTab.click()
+    await partnerInfoPage.searchText.fill('Alfara')
+    await partnerInfoPage.selectFirstOption()
+    const dropDownService = await partnerInfoPage.getServiceLocator()
+    await dropDownService.fill('Rft');
+    await expect(partnerInfoPage.succesNoResults).toHaveText('No Results FoundTry a new search.')
+    await partnerInfoPage.menu.click();
+})
