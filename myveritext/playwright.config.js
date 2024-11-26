0f55e931a4484dc7ef0d0e28e368e4e0f9dc92d3
Gustavo Guerrero <gguerrero@veritext.com>
2024-11-26 13:35:19 -0600

    Move MV-tests, managers, pageobjects, and utils into myveritext folder

--- myveritext/playwright.config.js
+++ myveritext/playwright.config.js
@@ -1,79 +1,52 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { devices } = require('@playwright/test');
+const { permission } = require('process');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
-module.exports = defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+const config = {
+  testDir: './myveritext/MV-tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+  
+    timeout: 5000
+  },
+  
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
-  /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name:'chrome',
+  use: {
+
+    browserName : 'chromium',
+    headless : false,
+    //viewport : {width: 720, height: 720},
+    screenshot : 'on',
+    ignoreHttpsErrors: false,
+    reporter: "allure-playwright",
+    Permissions:["gelocation"],
+    video: 'retain-on-failure',
+    trace : 'on',//off,on
+    //...devices["iPhone 14 Pro Max landscape"]
     },
+  },
+  //{
+   
+    //name:'webkitprofile',
+//use: {
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+  //browserName : 'webkit',
+  //headless : false,
+  //screenshot : 'on',
+  //trace : 'on',//off,on
+  //...devices['iPhone 11']
+  //},
+  
+//}
+]
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+};
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
-
+module.exports = config;
